82d828c0610b1c8192e21c40c08ed7617a4c89b7
Doghousedev <doghousedev@gmail.com>
2023-03-21 20:13:29 -0400

    once again we push it

--- src/components/Tests.svelte
+++ src/components/Tests.svelte
@@ -30,6 +30,7 @@
 		desc: string;
 		unit_cost: number;
 		unit_price: number;
+		selected: boolean;
 	}
 
 	let pbRecords: PbRecord[] = [
@@ -38,147 +39,168 @@
 			part_number: 'XBR-65X850E',
 			desc: '65" BRAVIA 4K HDR Ultra HD TV with Android TV',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 1299
+			unit_price: 1299,
+			selected: false
 		},
 		{
 			mfg: 'LG',
 			part_number: 'OLED65C7P',
 			desc: '65" OLED 4K HDR Smart TV',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 1499
+			unit_price: 1499,
+			selected: false
 		},
 		{
 			mfg: 'Panasonic',
 			part_number: 'TH-65EZ1000U',
 			desc: '65" OLED 4K HDR Ultra HD TV',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 2999
+			unit_price: 2999,
+			selected: false
 		},
 		{
 			mfg: 'Samsung',
 			part_number: 'UN65MU8500FXZA',
 			desc: '65" Curved 4K UHD Smart LED TV',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 1699
+			unit_price: 1699,
+			selected: false
 		},
 		{
 			mfg: 'Sharp',
 			part_number: 'PN-R903',
 			desc: '90" Class (90-1/16" diagonal) Professional LCD Display',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 8499
+			unit_price: 8499,
+			selected: false
 		},
 		{
 			mfg: 'NEC',
 			part_number: 'E905',
 			desc: '90" LED Backlit Commercial-Grade Display',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 7999
+			unit_price: 7999,
+			selected: false
 		},
 		{
 			mfg: 'Panasonic',
 			part_number: 'TH-98LQ70U',
 			desc: '98" Class (97.5" diagonal) Professional 4K UHD LCD Display',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 24999
+			unit_price: 24999,
+			selected: false
 		},
 		{
 			mfg: 'Sony',
 			part_number: 'FW-85BZ35F',
 			desc: '85" BRAVIA Professional Display with Android TV',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 4499
+			unit_price: 4499,
+			selected: false
 		},
 		{
 			mfg: 'LG',
 			part_number: '98LS95A-5B',
 			desc: '98" Ultra HD 4K Commercial Display with Smart Features',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 19999
+			unit_price: 19999,
+			selected: false
 		},
 		{
 			mfg: 'Samsung',
 			part_number: 'QH98H',
 			desc: '98" QLED 8K UHD Signage',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 69999
+			unit_price: 69999,
+			selected: false
 		},
 		{
 			mfg: 'Epson',
 			part_number: 'V11H812020',
 			desc: 'PowerLite 2150 Wireless Full HD 1080p 3LCD Projector',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 899
+			unit_price: 899,
+			selected: false
 		},
 		{
 			mfg: 'NEC',
 			part_number: 'P605UL',
 			desc: 'The P605UL entry level installation projector features advanced technologies at a competitive cost.',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 800 // replace with actual unit price as needed
+			unit_price: 800,
+			selected: false
 		},
 		{
 			mfg: 'Samsung',
 			part_number: 'QB65R',
 			desc: '65" commercial display with Edge-Lit LED technology',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 1500 // replace with actual unit price as needed
+			unit_price: 1500,
+			selected: false
 		},
 		{
 			mfg: 'Sony',
 			part_number: 'VPL-FHZ60L',
 			desc: 'The VPL-FHZ60 laser projector is ideal for a wide range of business, education and entertainment applications.',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 3000 // replace with actual unit price as needed
+			unit_price: 3000,
+			selected: false
 		},
 		{
 			mfg: 'NEC',
 			part_number: 'E506',
 			desc: 'Affordable 50-inch LCD display with full HD resolution',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 700 // replace with actual unit price as needed
+			unit_price: 700,
+			selected: false
 		},
 		{
 			mfg: 'LG',
 			part_number: '65SM8600PUA',
 			desc: '65" 4K UHD Smart LED NanoCell TV w/ AI ThinQ®',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 1200 // replace with actual unit price as needed
+			unit_price: 1200,
+			selected: false
 		},
 		{
 			mfg: 'Samsung',
 			part_number: 'ME95C',
 			desc: '95-inch LED backlit LCD commercial display with ultra-narrow bezel',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 15000 // replace with actual unit price as needed
+			unit_price: 15000,
+			selected: false
 		},
 		{
 			mfg: 'Sony',
 			part_number: 'VPL-VZ1000ES',
 			desc: 'The VPL-VZ1000ES ultra-short throw laser projector offers stunning 4K HDR picture quality from just inches away from the screen.',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 25000 // replace with actual unit price as needed
+			unit_price: 25000,
+			selected: false
 		},
 		{
 			mfg: 'Sharp',
 			part_number: 'PN-LE601',
 			desc: '60" Full HD Commercial LCD TV',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 800 // replace with actual unit price as needed
+			unit_price: 800,
+			selected: false
 		},
 		{
 			mfg: 'NEC',
 			part_number: 'E905',
 			desc: '90" large-screen LCD display with full HD resolution',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 12000 // replace with actual unit price as needed
+			unit_price: 12000,
+			selected: false
 		},
 		{
 			mfg: 'Sony',
 			part_number: 'FW-85BZ35F',
 			desc: '85" BRAVIA 4K HDR Professional Display',
 			unit_cost: Math.floor(Math.random() * 100) + 1,
-			unit_price: 3000 // replace with actual unit price as needed
+			unit_price: 3000,
+			selected: false
 		}
 	];
 
@@ -222,7 +244,12 @@
 						<TableBody style="max-height: 300px; overflow-y: auto;">
 							{#each pbRecords as pbRecord}
 								<TableRow>
-									<TableCell><Checkbox style="border-color:blue;" /></TableCell>
+									<TableCell
+										><Checkbox
+											bind:checked={pbRecord.selected}
+											style="border-color:blue;"
+										/></TableCell
+									>
 									<TableCell>{pbRecord.mfg}</TableCell>
 									<TableCell>{pbRecord.part_number}</TableCell>
 									<TableCell>{pbRecord.desc}</TableCell>
